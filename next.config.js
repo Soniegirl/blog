c3bd216b45c09b34e5d16f9abdd3a3721cf11665
Viktor Plane <viktorplane.sonie@gmail.com>
2022-07-17 20:47:51 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,4 +17,5 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   // Append the default value with md extensions
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  assetPrefix: '/blog/',
 });
